025745e954b884057454365ad45454d74721c8a0
ElizabethCG <elizabeth.caceres@gmail.com>
2019-05-30 02:40:31 -0400

    Agrega archivo index.html

---
+++ server.js
@@ -0,0 +1,14 @@
+//archivo,toma el localhost  y me muestra el index.html
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/src'));
+
+app.get('/',function(request, response){
+  response.sendFile(__dirname + '/index.html');
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, function(){
+  console.log(`Server Express on Port: ${port} Ready!`);
+});
